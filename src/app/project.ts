080bedb628d6a552d3a569658373af239b2a75cb
FestusMutie <mutiefestus84@gmail.com>
2021-05-20 06:12:12 +0300

    display list of projects

---
+++ src/app/project.ts
@@ -0,0 +1,3 @@
+export class Project {//class project passed as object
+constructor(public title: string, public description: string,public role: string,public date: string){}//constructor function for the class
+}
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Project} from '../project';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-
+projects:Project [] = [
+  new Project ('First project','Quote app development','UI design','20/05/2021'),
+  new Project ('Second project','To-d0-list app development','documentation','21/05/2021'),
+  new Project ('Third project','Portfolio website development','backend dev','20/04/2020')
+]
   constructor() { }
 
   ngOnInit(): void {
